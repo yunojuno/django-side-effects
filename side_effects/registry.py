9ea1384862d873713f24cfe5ce05ad62d176bb06
Hugo Rodger-Brown <hugo@yunojuno.com>
2020-11-30 13:53:20 +0000

    Remove deprecated providing_args parameter

--- side_effects/registry.py
+++ side_effects/registry.py
@@ -62,7 +62,8 @@ class Registry(defaultdict):
     # if using the disable_side_effects context manager or decorator,
     # then this signal is used to communicate details of events that
     # would have fired, but have been suppressed.
-    suppressed_side_effect = Signal(providing_args=["label"])
+    # RemovedInDjango40Warning: providing_args=["label"]
+    suppressed_side_effect = Signal()
 
     def __init__(self) -> None:
         self._lock = threading.Lock()
