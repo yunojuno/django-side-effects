3a85a39c5c0cb70178915c652ad8a592008dbb47
Hugo Rodger-Brown <hugorodgerbrown@users.noreply.github.com>
2022-08-31 14:33:48 +0100

    Drop Python 3.7, Django 2.2,3.0,3.1 (#21)

--- side_effects/settings.py
+++ side_effects/settings.py
@@ -13,14 +13,14 @@ def get_setting(setting_name: str, default_value: Any) -> Any:
 # If True then instead of logging exceptions in side-effects
 # functions, raise them, which will abort processing.
 # Default = False
-ABORT_ON_ERROR = get_setting("SIDE_EFFECTS_ABORT_ON_ERROR", False)  # type: bool
+ABORT_ON_ERROR: bool = get_setting("SIDE_EFFECTS_ABORT_ON_ERROR", False)
 
 # In test mode no side-effects are run
 # Default = False
-TEST_MODE = get_setting("SIDE_EFFECTS_TEST_MODE", False)  # type: bool
+TEST_MODE: bool = get_setting("SIDE_EFFECTS_TEST_MODE", False)
 
 # In FAIL test mode any call to run_side_effects will raise an Exception
 # This is used to uncover any tests that are running side-effects when
 # they shouldn't be.
 # Default = False
-TEST_MODE_FAIL = get_setting("SIDE_EFFECTS_TEST_MODE_FAIL", False)  # type: bool
+TEST_MODE_FAIL: bool = get_setting("SIDE_EFFECTS_TEST_MODE_FAIL", False)
--- tests/__init__.py
+++ tests/__init__.py
@@ -4,19 +4,19 @@ from side_effects.registry import _registry  # noqa: F401
 
 @has_side_effects("foo")
 def origin(message: str):
-    print(f"origin: {message}")  # noqa: T001
+    print(f"origin: {message}")  # noqa: T201
     return f"Message received: {message}"
 
 
 @is_side_effect_of("foo")
 def no_docstring(message: str):
-    print(f"side-effect.1: message={message}")  # noqa: T001
+    print(f"side-effect.1: message={message}")  # noqa: T201
 
 
 @is_side_effect_of("foo")
 def one_line_docstring(message: str):
     """This is a one-line docstring."""
-    print(f"side-effect.2: message={message}")  # noqa: T001
+    print(f"side-effect.2: message={message}")  # noqa: T201
 
 
 @is_side_effect_of("foo")
@@ -27,4 +27,4 @@ def multi_line_docstring(message: str, return_value=None):
     It has more information here.
 
     """
-    print(f"Side-effect.3: return_value={return_value}")  # noqa: T001
+    print(f"Side-effect.3: return_value={return_value}")  # noqa: T201
