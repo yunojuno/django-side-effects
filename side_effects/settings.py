e005f6ff0671731700ddb6255e92cbdb2e1556a5
Hugo Rodger-Brown <hugo@yunojuno.com>
2022-08-31 14:18:52 +0100

    Add logging for transaction scopes

--- side_effects/settings.py
+++ side_effects/settings.py
@@ -3,11 +3,10 @@ from __future__ import annotations
 from typing import Any
 
 from django.conf import settings
-from env_utils import get_bool
 
 
 def get_setting(setting_name: str, default_value: Any) -> Any:
-    return getattr(settings, setting_name, get_bool(setting_name, default_value))
+    return getattr(settings, setting_name, default_value)
 
 
 # If True then instead of logging exceptions in side-effects
@@ -24,3 +23,11 @@ TEST_MODE: bool = get_setting("SIDE_EFFECTS_TEST_MODE", False)
 # they shouldn't be.
 # Default = False
 TEST_MODE_FAIL: bool = get_setting("SIDE_EFFECTS_TEST_MODE_FAIL", False)
+
+# Controls the log level to use when warning if side-effects are running
+# inside an atomic transaction. This is not typically the desired
+# behaviour, as it can lead to a failure scenarios in which an exception
+# in an outer function (outside of run_side_effects) causes a tx
+# rollback when the side-effect itself cannot be reverted (e.g. sending
+# email), leaving system in an inconsistent state.
+ATOMIC_TX_LOG_LEVEL: str = get_setting("SIDE_EFFECTS_ATOMIC_TX_LOG_LEVEL", "debug")
