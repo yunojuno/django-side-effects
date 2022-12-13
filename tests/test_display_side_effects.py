61a289393c57f02ad7d5ab2ff9c56d8522c00136
coxm <coxm@users.noreply.github.com>
2022-12-13 14:53:29 +0000

    Add --sorted option to management command (#29)
    When specified, sorts by:
    
    *   function path for verbose and raw outputs;
    *   docstring for default output.

---
+++ tests/test_display_side_effects.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+from django.test import TestCase
+
+from side_effects.management.commands.display_side_effects import sort_events
+
+
+class SortEventsTests(TestCase):
+    def test_sort_events(self) -> None:
+        def handler_zero():
+            """Docstring 0."""
+
+        def handler_one():
+            """Docstring 1."""
+
+        def handler_two():
+            """Docstring 2."""
+
+        events = {
+            "label_2": [handler_zero, handler_one, handler_two],
+            "label_1": [handler_zero, handler_two],
+        }
+
+        sorted_by_function_name = list(
+            sort_events(
+                events,
+                handler_sort_key=lambda handler: handler.__name__,
+            ).items()
+        )
+        assert sorted_by_function_name == [
+            ("label_1", [handler_two, handler_zero]),
+            ("label_2", [handler_one, handler_two, handler_zero]),
+        ]
+
+        sorted_by_docstring = list(
+            sort_events(
+                events,
+                handler_sort_key=lambda handler: handler.__doc__,
+            ).items()
+        )
+        assert sorted_by_docstring == [
+            ("label_1", [handler_zero, handler_two]),
+            ("label_2", [handler_zero, handler_one, handler_two]),
+        ]
