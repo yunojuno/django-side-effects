a26fec6834961f517e4bff793b36d7eedcd07de8
Erdem Ozkol <erdem.ozkol@yunojuno.com>
2026-01-08 13:57:19 +0000

    Add Django 6.0, Python 3.13 support; drop Django 4.2, 5.0, Python <3.12

--- side_effects/registry.py
+++ side_effects/registry.py
@@ -60,7 +60,6 @@ class Registry(defaultdict):
     # if using the disable_side_effects context manager or decorator,
     # then this signal is used to communicate details of events that
     # would have fired, but have been suppressed.
-    # RemovedInDjango40Warning: providing_args=["label"]
     suppressed_side_effect = Signal()
 
     def __init__(self) -> None:
